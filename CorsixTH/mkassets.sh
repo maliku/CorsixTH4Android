d2edb9f177007ca46875cf3a9f61880c9dec4ac0
DongKai <dongkai329@yahoo.com.cn>
2012-05-10 16:18:15 +0800

    Change main menu background picture.

--- CorsixTH/mkassets.sh
+++ CorsixTH/mkassets.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 rm ../../../assets/game.zip > /dev/null
-zip -r ../../../assets/game.zip  Lua Levels Bitmap CorsixTH.lua
+zip -r ../../../assets/game.zip  Lua Levels Bitmap CorsixTH.lua -x Bitmap/mainmenu1080.bmp Bitmap/*.lua Bitmap/readme.txt
